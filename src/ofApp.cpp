70fc372351a1572f51b0cd27a6c3d0d226d406bd
A1Coder <username@users.noreply.github.com>
2017-03-29 02:00:25 -0300

    generated 20 uniform random coordinates

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,9 +1,41 @@
 #include "ofApp.h"
 
+using namespace std;
 //--------------------------------------------------------------
 void ofApp::setup(){
 jewel.load("diamond.png");// image to use (must be stored in "random/bin/data" folder)
-X = {500, 1000, 30, 100, 300, 200, 750, 380 };// set of x-coordinates for jewel
+
+//UNIFORM RANDOM COORDINATES
+
+//x-coordinates
+int x_min=0; 
+int x_max=1200;
+
+random_device rd;
+mt19937 en(rd());
+uniform_int_distribution < > my_listx(x_min,x_max);
+
+for(int i=0; i<20; i++)
+X.push_back(my_listx(en));
+
+
+
+//y-coordinates
+int y_min=0; 
+int y_max=970;
+uniform_int_distribution < > my_listy(y_min,y_max);
+
+for(int i=0; i<20; i++)
+Y.push_back(my_listy(en));
+
+
+/*int x_mean=1000;
+int x_sd=15;
+
+int y_mean=800;
+int y_sd=15;
+*/
+//X = {500, 1000, 30, 100, 300, 200, 750, 380 };// set of x-coordinates for jewel
 Y = {630, 400, 900,40, 200, 0, 90, 30}; //set of y-coordinates for jewel
 }
 
